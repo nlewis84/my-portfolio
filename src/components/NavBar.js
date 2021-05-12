88ee72b879e2c902ae2db9958d586390cde45c0b
nllew <n.lewis.perc@gmail.com>
2021-05-12 09:25:22 -0500

    added react social icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,66 @@
-import React from "react"
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function Navbar() {
-    return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
-                        Nathan
-                    </NavLink>
-                    <NavLink to="/post">
-                        Blog Posts
-                    </NavLink>
-                    <NavLink to="/project">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/about">
-                        About
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            Nathan
+          </NavLink>
+          <NavLink
+            to="/post"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            About
+          </NavLink>
+        </nav>
+        <div>
+          <SocialIcon
+            url="https://www.linkedin.com/in/nlewis84/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://github.com/nlewis84"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://twitter.com/nlewis84"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
