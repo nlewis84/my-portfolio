40bf52fc66d68a76b6a5acf633bc2c6f37709fb1
nllew <n.lewis.perc@gmail.com>
2021-05-12 13:13:02 -0500

    sanity studio files and navbar color swap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,64 +3,67 @@ import { NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
 
 export default function Navbar() {
-  return (
-    <header className="bg-red-600">
-      <div className="container mx-auto flex justify-between">
-        <nav className="flex">
-          <NavLink
-            to="/"
-            exact
-            activeClassName="text-white"
-            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
-          >
-            Nathan
-          </NavLink>
-          <NavLink
-            to="/post"
-            activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          >
-            Blog Posts
-          </NavLink>
-          <NavLink
-            to="/project"
-            activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/about"
-            activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          >
-            About
-          </NavLink>
-        </nav>
-        <div className="inline-flex py-3 px-3 my-6">
-          <SocialIcon
-            url="https://www.linkedin.com/in/nlewis84/"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 35, width: 35 }}
-          />
-          <SocialIcon
-            url="https://github.com/nlewis84"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 35, width: 35 }}
-          />
-          <SocialIcon
-            url="https://twitter.com/nlewis84"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 35, width: 35 }}
-          />
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="bg-gray-900">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink
+                        to="/"
+                        exact
+                        activeClassName="text-yellow-400"
+                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-yellow-400 text-4xl font-bold cursive tracking-widest"
+                    >
+                        Nathan
+                    </NavLink>
+                    <NavLink
+                        to="/post"
+                        activeClassName="text-yellow-400 bg-gray-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
+                    >
+                        Blog Posts
+                    </NavLink>
+                    <NavLink
+                        to="/project"
+                        activeClassName="text-yellow-400 bg-gray-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        activeClassName="text-yellow-400 bg-gray-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
+                    >
+                        About
+                    </NavLink>
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon
+                        url="https://www.linkedin.com/in/nlewis84/"
+                        className="mr-4"
+                        target="_blank"
+                        bgColor="#374151"
+                        fgColor="#FFF"
+                        style={{ height: 35, width: 35 }}
+                    />
+                    <SocialIcon
+                        url="https://github.com/nlewis84"
+                        className="mr-4"
+                        target="_blank"
+                        bgColor="#374151"
+                        fgColor="#FFF"
+                        style={{ height: 35, width: 35 }}
+                    />
+                    <SocialIcon
+                        url="https://twitter.com/nlewis84"
+                        className="mr-4"
+                        target="_blank"
+                        bgColor="#374151"
+                        fgColor="#FFF"
+                        style={{ height: 35, width: 35 }}
+                    />
+                </div>
+            </div>
+        </header>
+    );
 }
