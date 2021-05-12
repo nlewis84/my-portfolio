f5c411f40f346f52fa9128ff716133ccc610d53e
nllew <n.lewis.perc@gmail.com>
2021-05-12 13:56:05 -0500

    home color update

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
                 className="absolute object-cover w-full h-full"
             />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name">
                     Hey, I'm Nathan!
                 </h1>
             </section>
