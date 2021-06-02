186e3f44de0e8fc2d19333e1b0465e944f001bdd
nlewis <nathanlewis.dev@gmail.com>
2021-06-02 11:13:12 -0500

    added form and lambda

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
+import TuesdayTunes from "./components/TuesdayTunes";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <Switch>
-                <Route component={Home} path="/" exact />
-                <Route component={About} path="/about" />
-                <Route component={SinglePost} path="/post/:slug" />
-                <Route component={Post} path="/post" />
-                <Route component={Project} path="/project" />
-            </Switch>
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
+        <Route component={TuesdayTunes} path="/tt" />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/TuesdayTunes.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function TuesdayTunes() {
+  return (
+    <div>
+      <form name="contact" method="POST" data-netlify="true">
+        <div class="field">
+          <label class="label">
+            Your Name:
+            <input class="input" type="text" name="name" />
+          </label>
+        </div>
+        <div class="field">
+          <label class="label">
+            Your Email:
+            <input class="input" type="email" name="email" />
+          </label>
+        </div>
+        <div class="field">
+          <label for="role[]" class="label">
+            Your Role:
+          </label>
+          <div class="select is-multiple">
+            <select name="role[]" multiple size="2">
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">
+            Message:
+            <textarea class="textarea" name="message"></textarea>
+          </label>
+        </div>
+        <div class="field">
+          <button class="button is-primary is-medium" type="submit">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
