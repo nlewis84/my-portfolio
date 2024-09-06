c1d934cbdf2e2bb735213edcb10be20ef3476f9b
Nathan Lewis <nathan@differential.com>
2024-09-06 12:30:37 -0500

    chore: updated nav bar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,33 +5,36 @@ export default function Navbar() {
   return (
     <header className="bg-gray-900 fixed top-0 w-full z-10">
       <div className="container pl-4 flex justify-between">
-        <nav className="inline-flex text-left">
+        <nav className="inline-flex text-left text-indigo-50">
           <NavLink
-            to="/"
             exact
+            to="/"
             activeClassName="text-yellow-400"
-            className="inline-flex items-center py-6 px-3 mr-4 text-indigo-50 hover:text-yellow-400 text-4xl font-bold cursive tracking-widest"
+            className="inline-flex items-center py-6 px-3 mr-4 hover:text-yellow-400 text-4xl font-bold cursive tracking-widest"
           >
             Nathan
           </NavLink>
+
           <NavLink
             to="/post"
             activeClassName="text-yellow-400 bg-gray-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-indigo-50 hover:text-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-yellow-400"
           >
             Blog
           </NavLink>
+
           <NavLink
             to="/project"
             activeClassName="text-yellow-400 bg-gray-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-indigo-50 hover:text-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-yellow-400"
           >
             Projects
           </NavLink>
+
           <NavLink
             to="/about"
             activeClassName="text-yellow-400 bg-gray-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-indigo-50 hover:text-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-yellow-400"
           >
             About
           </NavLink>
